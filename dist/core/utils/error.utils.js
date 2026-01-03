9c4431da7b8a39bc5d1e62485ccbb4b73f95aa05
Roman <thalamus.rus@gmail.com>
2026-01-03 17:49:42 +0200

    add fixes for vercel deployment compatibility

---
+++ dist/core/utils/error.utils.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createErrorMessages = createErrorMessages;
+function createErrorMessages(errors) {
+    return { errorMessages: errors };
+}
