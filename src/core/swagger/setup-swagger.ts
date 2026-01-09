c7eb254502c2d08a44fa4232f8f3d99f80f5c162
Roman <thalamus.rus@gmail.com>
2026-01-10 00:17:02 +0200

    Fix swagger bugs

--- src/core/swagger/setup-swagger.ts
+++ src/core/swagger/setup-swagger.ts
@@ -1,21 +1,43 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import { Express } from 'express';
+import {Express} from 'express';
+import {join} from "node:path";
 
 const swaggerOptions = {
     definition: {
         openapi: '3.0.0',
         info: {
             title: 'Video hosting API',
-            version: '1.0.0',
-            description: 'video API',
-        }
+            version: '1.1.0',
+            description: 'video API'
+        },
+        tags: [
+            { name: 'Testing', description: 'API for clearing the database' },
+            { name: 'Videos', description: 'API for managing videos' }
+        ],
+        components: {
+            securitySchemes: {
+                basicAuth: {
+                    type: 'http',
+                    scheme: 'basic',
+                    description: 'Base64 encoded username and password',
+                }
+            }
+        },
+        security: [{basicAuth: []}]
     },
-    apis: ['../videos/docs/*.swagger.yml', './src/**/*.swagger.yml'],
+    apis: [
+        join(__dirname, '../videos/docs/testing.swagger.yml'),
+        join(__dirname, '../videos/docs/videos.swagger.yml')
+    ],
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 export const setupSwagger = (app: Express) => {
+    app.get('/api-json', (req, res) => {
+        res.json(swaggerSpec);
+    });
+
     app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
